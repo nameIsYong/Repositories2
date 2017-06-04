eb8c878b48adc2ffb1ecca855b16d17ffc9a4154
nameIsYong <1228359686@qq.com>
2017-06-04 09:29:55 +0800

    提交1

---
+++ MyOcProjectTest/MyOcProjectTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyOcProjectTest
+//
+//  Created by administrator on 2017/6/4.
+//  Copyright © 2017年 ydl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyOcProjectTest/MyOcProjectTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyOcProjectTest
+//
+//  Created by administrator on 2017/6/4.
+//  Copyright © 2017年 ydl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
